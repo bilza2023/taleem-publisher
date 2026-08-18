a203e53702c4bb3894dba1462e94513160b71b27
bilza2023 <bilza2023@gmail.com>
2026-08-18 20:02:28 +0500

    quick-ref course added

--- courses/index.js
+++ courses/index.js
@@ -1,7 +1,9 @@
 import fbise9math from "./fbise9math/fbise9math.json" with { type: "json" };
 import blog from "./blog/blog.json" with { type: "json" };
+import fbise9mathQuickRef from "./fbise9mathQuickRef/fbise9mathQuickRef.json" with { type: "json" };
 
 export default {
 	fbise9math,
-	blog
+	blog,
+	fbise9mathQuickRef
 };
--- scripts/test-publish.js
+++
@@ -1,5 +0,0 @@
-import TaleemPublish from "../src/publisher/TaleemPublish.js";
-
-const publisher = new TaleemPublish("fbise9math");
-
-await publisher.publish();
