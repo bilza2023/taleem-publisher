006fd7925b85eb2c3ab18afcabd1a9b1368706d7
bilza2023 <bilza2023@gmail.com>
2026-08-19 02:06:25 +0500

    new-schema

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,7 +1,8 @@
+///home/bilal-tariq/00--TALEEM/taleem-library/scripts/upload.js
 import fs from "fs";
 
 const source = "/home/bilal-tariq/00--TALEEM/taleem-library/prisma/dev.db";
-const target = "/home/bilal-tariq/00--TALEEM/taleem-server-prod/prisma/dev.db";
+const target = "/home/bilal-tariq/00--TALEEM/taleem-server-prod/prisma/content.db";
 
 fs.copyFileSync(source, target);
 
